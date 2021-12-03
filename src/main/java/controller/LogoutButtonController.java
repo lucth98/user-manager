1bab9430dead7ed8423e8f3946954f3ae10ffdc8
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2021-12-03 14:44:05 +0100

    + timer in LogoutButtonController that auto. logs out after 1min

--- src/main/java/controller/LogoutButtonController.java
+++ src/main/java/controller/LogoutButtonController.java
@@ -9,12 +9,21 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
-//TODO: Das System muss den Benutzer nach 60 Sekunden Inaktivität ausloggen
 
 public class LogoutButtonController implements ActionListener {
     private final MainFrame mainFrame;
     private final UserService userService;
+
+    private Timer timer=new Timer();
 
     public LogoutButtonController(MainFrame mainFrame)
     {
@@ -23,10 +32,38 @@ public class LogoutButtonController implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        logout();
+    }
+
+    private void logout() {
         userService.logout(mainFrame.getAccountUsernameField().getText());
         mainFrame.getMainPane().setEnabledAt(MainFrame.ACCOUNT_PANE_INDEX, false);
         mainFrame.getMainPane().setEnabledAt(MainFrame.LOGIN_PANE_INDEX, true);
         mainFrame.getMainPane().setEnabledAt(MainFrame.REGISTER_PANE_INDEX, true);
         mainFrame.getMainPane().setSelectedIndex(MainFrame.LOGIN_PANE_INDEX);
     }
+
+
+    //TODO: calling start & reset
+    private void startTimer(){
+        TimerTask timerTask =new TimerTask() {
+            @Override
+            public void run() {
+                logout();
+            }
+        };
+
+        //setting date = now +1 min
+        LocalDateTime localDateTime=LocalDateTime.now().plus(Duration.of(1, ChronoUnit.MINUTES));
+        Date date=Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+
+        timer.schedule(timerTask,date);
+    }
+
+    private void resetTimer(){
+        timer.cancel();
+        startTimer();
+    }
+
+
 }
